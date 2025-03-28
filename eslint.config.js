05b4086b5f7b2776477d2caffff09fdda90558eb
PrisT-11 <priscillathung11@gmail.com>
2025-03-28 15:45:51 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from "eslint";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-export default defineConfig([
+export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+];
